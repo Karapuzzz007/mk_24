3fe2d670fae5099fa7e11cf30713880355c7cf27
SafronovIA <148233033+Karapuzzz007@users.noreply.github.com>
2024-02-15 17:52:26 +0300

    Delete build directory

--- build/CMakeFiles/libopencm3_lib.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/libopencm3_lib"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/libopencm3_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/libopencm3_lib.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for libopencm3_lib.
-# This may be replaced when dependencies are built.
