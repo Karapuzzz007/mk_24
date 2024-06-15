1c24d477b8920161f4634f52e0c386d1ccdecf80
Сафронов Иван <forrestwashere007@gmail.com>
2024-06-15 11:37:36 +0300

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,10 @@
 #include <libopencm3/cm3/nvic.h>
 #include <libopencm3/stm32/usart.h>
 
+uint8_t str[20]{'a'}; //?
+
+//Ring_buffer buf; 
+
 static void my_usart_print_int( int16_t value)
 {
 	int8_t i;
@@ -30,7 +34,7 @@ static void my_usart_print_int( int16_t value)
 }
 
 void indicate_adc_val(uint32_t val){
-if (val > 2048) gpio_set(GPIOE, GPIO11);
+if (val > 1024) gpio_set(GPIOE, GPIO11);
 else gpio_clear(GPIOE, GPIO11);
 }
 
@@ -42,7 +46,7 @@ uint32_t start_wait_and_read_adc(){
 }
 
 void blocking_delay_parrots () {
-    for (volatile uint32_t i = 0; i < 200000; i++); 
+    for (volatile uint32_t i = 0; i < 500000; i++); 
 }
 
 void setup_ADC () {
@@ -65,23 +69,29 @@ void setup_ADC () {
 }
 
 void setup_USART (){
-// Интерфейс U(S)ART с внешним миром
-rcc_periph_clock_enable(RCC_GPIOA);                           // Разморозка порта ввода/вывода
+    // Интерфейс U(S)ART с внешним миром
+    rcc_periph_clock_enable(RCC_GPIOA);                           // Разморозка порта ввода/вывода
+    //rcc_periph_clock_enable(RCC_GPIOE);
 
-gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO2 | GPIO3);  // Режим альтернативной функции
-gpio_set_af(GPIOA,GPIO_AF7, GPIO2 | GPIO3);                           // Альтернативная функция (выбор по номеру) PA9 --- Tx, PA10 --- Rx.
+    gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO2 | GPIO3);  // Режим альтернативной функции
+    gpio_set_af(GPIOA,GPIO_AF7, GPIO2 | GPIO3);                           // Альтернативная функция (выбор по номеру) PA9 --- Tx, PA10 --- Rx.
 
-rcc_periph_clock_enable(RCC_USART2);                      // Разморозка ПУ
+    //gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9|GPIO11);
 
-usart_set_baudrate(USART2, 19200);                       // Скорость передачи
-usart_set_databits(USART2, 8);                            // Размер посылки
-usart_set_stopbits(USART2, USART_STOPBITS_1);             // Количество стоп-битов
-usart_set_parity(USART2, USART_PARITY_NONE);              // Контроль четности
+    rcc_periph_clock_enable(RCC_USART2);                      // Разморозка ПУ
 
-usart_set_mode(USART2, USART_MODE_TX_RX);                 // Режим работы ПУ
-usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);   // Управление процессом передачи сообщений
+    usart_set_baudrate(USART2, 19200);                       // Скорость передачи
+    usart_set_databits(USART2, 8);                            // Размер посылки
+    usart_set_stopbits(USART2, USART_STOPBITS_1);             // Количество стоп-битов
+    usart_set_parity(USART2, USART_PARITY_NONE);              // Контроль четности
 
-usart_enable(USART2);                                     // Включение ПУ
+    usart_set_mode(USART2, USART_MODE_TX_RX);                 // Режим работы ПУ
+    usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);   // Управление процессом передачи сообщений
+
+    //usart_enable_rx_interrupt(USART2);
+    //nvic_enable_irq(NVIC_USART2_EXTI26_IRQ);
+
+    usart_enable(USART2);                                     // Включение ПУ
 }
 
 void setup () {
@@ -110,6 +120,7 @@ while(desyat>0){
     t++;
 }
 t--;
+uint8_t res_str[7];
 
 blocking_delay_parrots();
 gpio_toggle(GPIOE, GPIO9);
